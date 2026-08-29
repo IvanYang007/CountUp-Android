e891c9c28f3478bad277aceaaae8d58ec31956c6
Ivan Pi <xn31415@gmail.com>
2026-08-29 19:35:59 -0400

    chore(release): bump version to 1.4.0 (versionCode 7)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.countup.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 6
-        versionName = "1.3.0"
+        versionCode = 7
+        versionName = "1.4.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
