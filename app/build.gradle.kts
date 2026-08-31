22e523c2c403db3543a2af5bbe3e0e508ec86811
Ivan Pi <xn31415@gmail.com>
2026-08-31 08:23:55 -0400

    chore(release): bump version to 1.4.6 (versionCode 13) for Play Store upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.countup.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 12
-        versionName = "1.4.5"
+        versionCode = 13
+        versionName = "1.4.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
