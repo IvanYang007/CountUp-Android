592b72b7cdae62033f901d296dda682d3adf328e
Ivan Pi <xn31415@gmail.com>
2026-09-20 21:50:50 -0400

    chore(release): bump versionCode to 63 (v3.1.1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.countup.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 62
-        versionName = "3.1.0"
+        versionCode = 63
+        versionName = "3.1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
